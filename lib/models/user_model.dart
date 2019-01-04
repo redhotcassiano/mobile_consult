4ba5a9cc62b7539a1c0b2eaffd828029f81cde80
Cassiano de Souza e Silva <cassiano.silva@inone.com.br>
2019-01-04 17:25:55 -0200

    Correção.

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -16,17 +16,17 @@ class UserModel extends Model {
     isLoading = true;
     notifyListeners();   
 
-    const baseUrlApi = "http://www.megaredhot.com:8080/api";
-    
-    final header =  {
-      HttpHeaders.authorizationHeader: 'Bearer ' + "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImEzNGIxNTdhMDk5MWY2YzE4ZmExN2RkMzkyNTVmMzkxNTI5NmM1NDc5MDYyZTQwZGJkODgxNDFjODkyZTY5NWJjNThkYzgxMjcyMTM0OGZmIn0.eyJhdWQiOiI0IiwianRpIjoiYTM0YjE1N2EwOTkxZjZjMThmYTE3ZGQzOTI1NWYzOTE1Mjk2YzU0NzkwNjJlNDBkYmQ4ODE0MWM4OTJlNjk1YmM1OGRjODEyNzIxMzQ4ZmYiLCJpYXQiOjE1NDU4MzA1NDUsIm5iZiI6MTU0NTgzMDU0NSwiZXhwIjoxNTc3MzY2NTQ1LCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.toK8TIEEMMLoxuvQFptdVLAhJKLemIiEUdV3kuvfN2rN5ZzIeNZ7T2uJnVQoL09xAWQnNThkQctDeFxcO1dBqB1SIrY-9JFzv_iiVB77ViIvynf9v2KQytmDh6Ei9FKYVAc60JwMK_-7jYuryfJHfIVr8VX_ydpDq21XqJgZQO1KqQlqfOa3wUVROjRh3ATU3EzDG7Kxxa8a8Vz08P84K1qzntWTRUzjUGdGE62J2yUEUTq1yIZj8GqUFuVQd622ULqiA1qQZSwAxzJEsF836kxJtH9ZQp-NaJmgq1eT55o1czbCAdMg8wAqft6aKx0SLGERrEEwYGPSQ1s0Sy_xdS-ac31g1PpJGb8Py5awx_GCVlZHT6o5tubdNHJKH8SkZHWPeXFIOMNrwKc6G_wGX7f1SqpyUG4aymZf5uB9N7EPF4Erhj_aMdfqxRDOBjklwRI9eJX2bFPrqvLZxYut5foDg7hO8chazhaB3NqvQmx7XqFqhrcEauh7jtfZCfhTwv5IgOwujJcX4vEB_2AoEqUCIuH6XiEbbRlO-XVXvhWLBODdr_-XbQqWHs_sBZXe2AiJ9AMGZCRazCKNsw6__FGrWAO4bzk-zm1VzozGzDssBlQwY8GXlrll5Ql5uKJbfFGzGZzFK8WqtdEV3_VzAUAJ-lbbqF-ih0xdlbJqy-E", 
-      HttpHeaders.acceptHeader: 'application/json',
-      HttpHeaders.contentTypeHeader : 'application/x-www-form-urlencoded'
-    };
+    const baseUrlApi = "http://www.megaredhot.com:8080/api";   
 
     final data =  await authApp();  
 
     final Map<String, dynamic> authData = data; 
+     final apiToken = authData['access_token'].toString();
+     final header =  {
+      HttpHeaders.authorizationHeader: 'Bearer ' + apiToken, 
+      HttpHeaders.acceptHeader: 'application/json',
+      HttpHeaders.contentTypeHeader : 'application/x-www-form-urlencoded'
+    };
 
     print('---------------- LOGADO ---------------------');
     print(authData['access_token']);
@@ -44,7 +44,7 @@ class UserModel extends Model {
       'profiles_id' : '5'
     }, headers: header).then((http.Response response) {
       print("Response status: ${response.statusCode}");
-      print("Response body: ${response.contentLength}");
+      print("Response header: ${header}");
       print(response.body);
       print(response.request);
 
@@ -101,27 +101,27 @@ class UserModel extends Model {
     
     const baseUrl = "http://www.megaredhot.com:8080/";
     const baseUrlApi = "http://www.megaredhot.com:8080/api";
-    const client_id = '5';
-    const client_secret = 'Eydmlllzi51LEXIEkdU95cnRyl7COcJS3nWesDiI';
+    const client_id = '9';
+    const client_secret = 'CJekmDnHOhs9NYkdvU5ROqCuaXL9VThPZBtdMPCB';
+    const urlCallback = 'http://localhost/callback';
     const user = 'admin@email.com';
     const pass = '123456';     
 
     final response = await http.post(baseUrl + "/oauth/token", body: {
-      'grant_type'    : 'password',
+      'grant_type'    : 'client_credentials',
       'client_id'     : client_id,
-      'client_secret' : client_secret,
-      'username'      : user,
-      'password'      : pass,
+      'client_secret' : client_secret,      
       'scope'         : '*'
     });
+    
     
     return json.decode(response.body);    
   }
 
   authApp () async {    
     const baseUrl = "http://www.megaredhot.com:8080/";
-    const client_id = '4';
-    const client_secret = '2NPIwvhsOG8IMU2F5ZAaIzPQ1fRCcI7wl2YgRkDc';
+    const client_id = '9';
+    const client_secret = 'CJekmDnHOhs9NYkdvU5ROqCuaXL9VThPZBtdMPCB';
 
     final response = await http.post(baseUrl + "/oauth/token", body: {
       'grant_type'    : 'client_credentials',
